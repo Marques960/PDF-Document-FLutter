547bb894588130b462352692c20f7cdfe7d04a8b
Marques960 <rafaeltavaresmarques06@gmail.com>
2024-06-18 13:31:15 +0100

    pdf extension

--- pdfwebapp/lib/pdf_page1.dart
+++ pdfwebapp/lib/pdf_page1.dart
@@ -1,5 +1,5 @@
 //ignorances
-// ignore_for_file: depend_on_referenced_packages 
+// ignore_for_file: depend_on_referenced_packages, avoid_print, unused_element
 // ignore_for_file: unused_local_variable
 // ignore_for_file: prefer_const_constructors
 // ignore_for_file: camel_case_types
@@ -9,10 +9,10 @@
 // ignore_for_file:
 // ignore_for_file:
 
-
 //imports
 import 'package:flutter/material.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:pdfwebapp/style.dart';
 import 'package:printing/printing.dart';
 import 'package:pdf/pdf.dart';
 
@@ -24,6 +24,12 @@ class Pdfpage extends StatefulWidget {
 }
 
 class _pdfpageState extends State<Pdfpage> {
+
+  String var1 = "func1";
+  String var2 = "func2";
+  String var3 = "func3";
+  String var4 = "func4";
+  String var5 = "func5";
 
   // Method for printing the PDF
   Future<void> _printPdf() async {
@@ -37,19 +43,72 @@ class _pdfpageState extends State<Pdfpage> {
         build: (pw.Context context) {
           return pw.Center(
             child: pw.Container(
-              width: (screenWidth),
+              width: screenWidth,
               height: screenHeight,
-              color : PdfColor.fromHex("#E0E0E0"),
+              color: PdfColor.fromHex("#E0E0E0"),
               child: pw.Center(
                 child: pw.Column(
                   children: [
-                    pw.Container(
-                      height: 200,
-                      child: pw.CustomPaint(
-                        size: PdfPoint(300, 300),
-                        painter: drawBarChart, 
+                    pw.SizedBox(height: 20),
+                    //header of the page (optional)
+                    pw.Text(
+                      "Gestão RFS",
+                      style: pw.TextStyle(
+                        fontSize: 18,
                       ),
                     ),
+                    //define the content for your pdf page
+                    pw.SizedBox(height: 20),
+                    pw.Container(
+                      child: drawBarChart(
+                          PdfPoint((screenWidth / 5), (screenHeight / 3))),
+                    ),
+                    pw.Container(
+                      alignment: pw.Alignment.center,
+                      margin: pw.EdgeInsets.only(top: 10),
+                      child: pw.Row(
+                        children: [
+                          pw.SizedBox(width: 80),
+                          pw.Padding(
+                            padding: pw.EdgeInsets.only(left: 30),
+                            child:  pw.Text(
+                              var1,
+                              style: textStyle,
+                            ),
+                          ),
+                          pw.Padding(
+                            padding: pw.EdgeInsets.only(left: 20),
+                            child:  pw.Text(
+                              var2,
+                              style: textStyle,
+                            ),
+                          ),
+                          pw.Padding(
+                            padding: pw.EdgeInsets.only(left: 20),
+                            child:  pw.Text(
+                              var3,
+                              style: textStyle,
+                            ),
+                          ),
+                          pw.Padding(
+                            padding: pw.EdgeInsets.only(left: 20),
+                            child:  pw.Text(
+                              var4,
+                              style: textStyle,
+                            ),
+                          ),
+                          pw.Padding(
+                            padding: pw.EdgeInsets.only(left: 20),
+                            child:  pw.Text(
+                              var5,
+                              style: textStyle,
+                            ),
+                          ),
+                          pw.Spacer(),
+                        ],
+                      ),
+                    ),
+                    pw.Spacer(),
                   ],
                 ),
               ),
@@ -63,21 +122,42 @@ class _pdfpageState extends State<Pdfpage> {
     );
   }
 
-  //pdf
-  void drawBarChart(PdfGraphics canvas, PdfPoint size) {
-    final data = [50, 40, 30, 20, 10];
+  pw.Widget drawBarChart(PdfPoint size) {
+    final data = [100, 30, 60, 40, 75];
     final barWidth = size.x / (2 * data.length);
+    final List<String> teste = [
+      "Rafael",
+      "Rafael",
+      "Rafael",
+      "Rafael",
+      "Rafael",
+      "Rafael",
+      "Rafael"
+    ];
     final maxDataValue = data.reduce((a, b) => a > b ? a : b);
 
-    for (int i = 0; i < data.length; i++) {
-      final barHeight = (data[i] / maxDataValue) * size.y;
-      final x = i * 2 * barWidth;
-      final int y = 0;
-      canvas.drawRect(x, y as double, barWidth, barHeight);
-      canvas.setFillColor(PdfColors.blue);
-      canvas.fillPath();
-    }
+    return pw.Container(
+      width: size.x,
+      height: size.y,
+      child: pw.Stack(
+        children: [
+          for (int i = 0; i < data.length; i++)
+            pw.Positioned(
+              left: i * 2 * barWidth,
+              bottom: 0,
+              child: pw.Container(
+                width: barWidth,
+                height: (data[i] / maxDataValue) * size.y,
+                color: PdfColors.blue,
+              ),
+            ),
+        ],
+      ),
+    );
   }
+
+  // Save the PDF file
+  // You need to handle saving the PDF file as per your requirements
 
   @override
   Widget build(BuildContext context) {
@@ -91,10 +171,10 @@ class _pdfpageState extends State<Pdfpage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             FloatingActionButton(
-            onPressed: _printPdf,
-            tooltip: 'Print',
-            child: const Icon(Icons.picture_as_pdf),
-          ),
+              onPressed: _printPdf,
+              tooltip: 'Print',
+              child: const Icon(Icons.picture_as_pdf),
+            ),
           ],
         ),
       ),
---
+++ pdfwebapp/lib/style.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: depend_on_referenced_packages, non_constant_identifier_names, constant_identifier_names, unused_import
+
+import 'package:flutter/material.dart';
+import 'package:pdf/widgets.dart' as pw;
+
+final pw.TextStyle textStyle = pw.TextStyle(
+  font: pw.Font.helvetica(),
+  fontSize: 12,
+);
